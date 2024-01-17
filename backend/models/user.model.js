da133a08f8e209f6de6d4ecebff842af96921f4d
Pranay <pmcanvas4501@gmail.com>
2024-01-17 12:50:57 +0530

    backend link is changed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String},
     membership: { type: String, required: true, default: "Basic" },
     postsCount: { type: Number, default: 0 },
     lastPostTimestamp: { type: Date, default: Date.now },
