74fc5eb2bfe7ddd3205b496201a2ea9c5a2fe0a2
Pranay <pmcanvas4501@gmail.com>
2024-01-26 22:52:39 +0530

    ui is changed

--- frontend/src/components/pageLoading/PageLoading.jsx
+++ frontend/src/components/pageLoading/PageLoading.jsx
@@ -4,8 +4,9 @@ import "./pageloading.css";
 export default function PageLoading() {
   return (
     <div className="vh-100 d-flex justify-content-center align-items-center w-100">
-      <div class="centre">
-        <div class="loader"></div>
+      <div className="loader-container">
+        <div className="loader"></div>
+        <div className="loader-text">Loading...</div>
       </div>
     </div>
   );
